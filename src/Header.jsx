f1b9ae89946179b27b536874cc230607f55028f4
bd6981 <brittanyjdavis22@gmail.com>
2022-12-08 09:59:25 -0500

    added intro file

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="Header">
+        <header>
+            <h1>Our Connections</h1>
+
+        </header>
+    </div>
+  )
+}
+
+export default Header
