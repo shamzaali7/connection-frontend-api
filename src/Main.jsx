52e25e0fe8f11f596ae85301c17376f53d915d1b
CaseyDragon <cndragon83@gmail.com>
2022-12-08 18:55:29 -0500

    create contact component

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import './App.css'
 import './Main.css'
+import './data.json'
+
+
 
 const Main = () => {
   return (
-    <div className="postItBoard">
-    {/* <div id='#'> */}
-      <div className="postIt"></div>
-      <div className="postIt"></div>
-      <div className="postIt"></div>
-      <div className="postIt"></div>
-    {/* </div> */}
-    </div>
-  )
-}
-
-export default Main
+    async function getContacts() {
+      fetch('./data.json')
+        .then(function(resp) {
+          return resp.json();
+        })
+        .then(function(contacts) {
+          console.log (contacts)
+     //above DELETE  using fetch  to get json 
+     //Below KEEP map to create postits  
+          contacts.map((contact)=> {
+      console.log(contact)
+      return (
+        <div key={contact._id} className="postIt" id={contact._id}>
+          <p>{contact.name} <br/> Connection Notes:<br/>{contact.connection}<br/>
+          {contact.email} <br/> {contact.github}{contact.linkedIn}</p>
+        </div>
+      )
+    })
+  })
+    })
+};
+  export default Main 
+ 
