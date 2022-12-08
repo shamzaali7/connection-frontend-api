bd959b3b76b1fd4a8a2e7b63cc41f85cb0b656ec
bd6981 <brittanyjdavis22@gmail.com>
2022-12-08 12:20:02 -0500

    push updated nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,12 +5,13 @@ import { FcAbout } from 'react-icons/fc';
 import { FcHome } from 'react-icons/fc';
 
 
+
 const Nav = () => {
   const [activeNav, setActiveNav] = useState("#")
   return (
     <nav>
-      <a href="#" onClick= {() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><FcHome/></a>
-      <a href="#about" onClick= {() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FcAbout/></a>
+      <a href="main" onClick= {() => setActiveNav('main')} className={activeNav === "main" ? 'active' : ''}><FcHome size={35}/></a>
+      <a href="#about" onClick= {() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FcAbout size={35}/></a>
       
       
       
