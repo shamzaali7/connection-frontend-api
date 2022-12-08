7f7301e4a3499fb351f3de69234f694943864fa4
bd6981 <brittanyjdavis22@gmail.com>
2022-12-08 09:56:23 -0500

    added Header footer and Main files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import react from 'react';
 import './App.css';
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header/>
+      <Main/>
+      <Footer/>
       
     </div>
   );
