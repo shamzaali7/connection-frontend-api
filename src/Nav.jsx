a2d01b0eae74549fb8eb96c73ad6a5890525f480
bd6981 <brittanyjdavis22@gmail.com>
2022-12-13 17:11:21 -0500

    link nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,6 +3,7 @@ import "./Nav.css"
 import {useState} from 'react'
 import { FcAbout } from 'react-icons/fc';
 import { FcHome } from 'react-icons/fc';
+import Intro from './Intro'
 
 
 
@@ -11,7 +12,7 @@ const Nav = () => {
   return (
     <nav>
       <a href="main" onClick= {() => setActiveNav('main')} className={activeNav === "main" ? 'active' : ''}><FcHome size={35}/></a>
-      <a href="#about" onClick= {() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FcAbout size={35}/></a>
+      <a href="Intro" onClick= {() => setActiveNav('Intro')} className={activeNav === '/Intro' ? 'Intro' : ''}><FcAbout size={35}/></a>
       
       
       
