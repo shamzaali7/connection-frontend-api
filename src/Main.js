981e7f34205e793a58b92fc43c5404a877fbf3cb
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-13 16:28:10 -0500

    contacts css updated

--- src/Main.js
+++ src/Main.js
@@ -49,23 +49,25 @@ const Main = () => {
                     
                   <header1 className="name">{contact.name}</header1> 
                   <form className="postItNotes">
-                  <div>
-                  <h4>Connection Notes:</h4>
-                  <Box>{contact.connection}</Box>
-                  </div></form>
-                  <form>
-                  <Box>
-                  <a href={contact.email}><AiOutlineMail color="black" fontSize="2em" /></a>
-                  <a href={contact.github}><AiFillGithub color="black" fontSize="2em" /></a>
-                  <a href={contact.linkedIn}><AiFillLinkedin color="black" fontSize="2em" /></a>
-                  </Box>
-                  
+                    <div className="connection-container">
+                      <div className="connection-notes"><h4>Connection Notes:</h4></div>
+                      <div><Box>{contact.connection}</Box></div>
+                    </div>
                   </form>
-                  <p ><Link to={'/main/' + contact._id}><button>Edit</button></Link></p>
-                  {/* <Update contact={contact}><button>Edit</button></Update>  */}
-                 
+                  <form>
+                    <Box>
+                      <div className="links-container">
+                        <div><a href={contact.email}><AiOutlineMail color="black" fontSize="2em" /></a></div>
+                        <div><a href={contact.github}><AiFillGithub color="black" fontSize="2em" /></a></div>
+                        <div><a href={contact.linkedIn}><AiFillLinkedin color="black" fontSize="2em" /></a></div>
+                      </div>
+                    </Box>
+                  </form>
+                  <div className="edit-button-container">
+                    <div><Link to={'/main/' + contact._id}><button className="edit-button">Edit</button></Link></div>
                   </div>
-                  
+                  {/* <Update contact={contact}><button>Edit</button></Update>  */}
+                  </div>   
                 )
             })}
           </div>
