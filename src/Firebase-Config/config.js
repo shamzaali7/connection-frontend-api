359ee80a4cd8e60cf5ea7f3e2b525e57cb55de74
bd6981 <brittanyjdavis22@gmail.com>
2022-12-13 15:49:22 -0500

    login work

--- src/Firebase-Config/config.js
+++ src/Firebase-Config/config.js
@@ -17,22 +17,73 @@
 //below is the code to paste to your Application
 // the following code need to be hidden by envirement varibles dont publish without hiding  
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+  useAuth
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnQJ5e_PdDGXw_doCp3dW-UnjlSu4wOds",
-  authDomain: "connections-group.firebaseapp.com",
-  projectId: "connections-group",
-  storageBucket: "connections-group.appspot.com",
-  messagingSenderId: "580383101801",
-  appId: "1:580383101801:web:389585bbdc7f86ee8ea3f8",
-  measurementId: "G-RTHPVT8VWK"
+    apiKey: "AIzaSyBnQJ5e_PdDGXw_doCp3dW-UnjlSu4wOds",
+    authDomain: "connections-group.firebaseapp.com",
+    projectId: "connections-group",
+    storageBucket: "connections-group.appspot.com",
+    messagingSenderId: "580383101801",
+    appId: "1:580383101801:web:389585bbdc7f86ee8ea3f8",
+    measurementId: "G-RTHPVT8VWK"
+  };
+//   now write this code 
+ 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+     
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
 };
 
-const app = initializeApp(firebaseConfig)
+const logout = () => {
+  signOut(auth);
+};
 
-export const auth = getAuth(app)
+export {
+  auth,
+
+  db,
+  signInWithGoogle,
+  logout,
+};
 
 //   Now go back to you firebase main screen and click on authentication
 // click on sign in method
