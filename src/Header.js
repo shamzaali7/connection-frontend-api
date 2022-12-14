55e7b00a6a20258b332a0a346c950d4665da1589
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-14 11:17:46 -0500

    align center

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ import './App.css'
 const Header = () => {
   return (
     <div className="Header">
-        <header>
+        <header className="header-title">
             <h1>The Connections Keeper</h1>
             
         </header>
