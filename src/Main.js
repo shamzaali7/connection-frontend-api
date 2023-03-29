904acb5b79d6576fabad9b12b4180890e92188d3
Hamzaali7 <Hamzaali7@yahoo.com>
2023-03-29 14:55:22 -0400

    spacing

--- src/Main.js
+++ src/Main.js
@@ -13,8 +13,6 @@ import { AiFillLinkedin } from 'react-icons/ai';
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "./Firebase-Config/AuthContext";
 
-
-
 const Main = () => {
   const { user, logout } = UserAuth();
   const navigate = useNavigate();
@@ -107,12 +105,10 @@ const Main = () => {
           <div className="max-w-[600px] mx-auto my-16 p-4">
             <h1 className="text-2xl font-bold py-4">Account</h1>
             <p>User Email: {user && user.email}</p>
-
             <button onClick={handleLogout} className="border px-6 py-2 my-4">
               Logout
             </button>
           </div>
-          
         </div>
       );
   };
